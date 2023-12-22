cb8b02e46e62c7f1a9da10c112470c1e402a7da6
Benjamin St. Amour <b.st.amour6@gmail.com>
2023-12-22 00:45:39 -0500

    added three more attacks to final room

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -6,6 +6,11 @@ public class BattleManager : MonoBehaviour
 {
 	private bool activated = false;
 	
+	// ENTRY SINK
+	[SerializeField] GameObject entryTiles;
+	[SerializeField] float sinkSpeed = 1;
+	[SerializeField] Vector3 sinkDist = new Vector3(0, -0.5f, 0);
+	
 	// BATTERING RAM ATTACK
 	[SerializeField] GameObject[] batteringRams;
 	[SerializeField] int numSmashes = 8;
@@ -23,6 +28,27 @@ public class BattleManager : MonoBehaviour
 	[SerializeField] GameObject sweepingLasers2;
 	[SerializeField] float lasers2Speed;
 	[SerializeField] Vector3 lasers2Dist = new Vector3(0, 0, 61);
+	
+	// SWEEPING LASERS ATTACK 3
+	[SerializeField] GameObject sweepingLasers3;
+	[SerializeField] float lasers3Speed;
+	[SerializeField] Vector3 lasers3Dist = new Vector3(0, 0, 48.25f);
+	[SerializeField] GameObject sideLaser;
+	[SerializeField] float sideLaserSpeed;
+	[SerializeField] Vector3 sideLaserDist = new Vector3(21, 0, 0);
+	
+	// LASER CIRCLE ATTACK
+	[SerializeField] GameObject[] laserCircles;
+	[SerializeField] int numCircles = 6;
+	
+	// COLOUR-CODED SPIKE DROP ATTACK
+	[SerializeField] GameObject redSpikes;
+	[SerializeField] GameObject blueSpikes;
+	[SerializeField] GameObject greenSpikes;
+	[SerializeField] float spikeDropSpeed;
+	[SerializeField] Vector3 spikeDropDist = new Vector3(0, -7.875f, 0);
+	[SerializeField] GameObject colourLights;
+	
 	
     // Start is called before the first frame update
     void Start()
@@ -42,7 +68,27 @@ public class BattleManager : MonoBehaviour
 		{
 			activated = true;
 			
-			StartCoroutine(laserAttack1());
+			StartCoroutine(sinkEntry());
+			
+			//StartCoroutine(colourSpikeDrop());
+			
+			int[] circleSpawns = new int[numCircles];
+			for(int i = 0; i < numCircles; i++)
+			{
+				int index = UnityEngine.Random.Range(0, laserCircles.Length);
+				if(i > 0)
+				{
+					while(System.Array.IndexOf(circleSpawns, index) > -1)
+					{
+						index = UnityEngine.Random.Range(0, laserCircles.Length);
+					}
+				}
+				circleSpawns[i] = index;
+			}
+			
+			StartCoroutine(laserCircleSpawn(circleSpawns, 0));
+			
+			//StartCoroutine(laserAttack3());
 			
 			/*int[] attackRams = new int[numSmashes];
 			for(int i = 0; i < numSmashes; i++)
@@ -61,8 +107,167 @@ public class BattleManager : MonoBehaviour
 		}
 	}
 	
+	IEnumerator sinkEntry()
+	{
+		Vector3 startPos = entryTiles.transform.position;
+		while(true)
+		{
+			entryTiles.transform.position = Vector3.MoveTowards(entryTiles.transform.position, startPos + sinkDist, sinkSpeed*Time.deltaTime);
+			if((entryTiles.transform.position - (startPos + sinkDist)).magnitude <= 0.01)
+			{
+				entryTiles.transform.position = startPos + sinkDist;
+			}
+			if(entryTiles.transform.position == startPos + sinkDist)
+			{
+				break;
+			}
+			else
+			{
+				yield return null;
+			}
+		}
+		entryTiles.SetActive(false);
+		
+		//yield return new WaitForSeconds(1);
+		
+		//StartCoroutine(laserAttack1());
+	}
+	
+	IEnumerator colourSpikeDrop()
+	{
+		string[] arr = {"R", "R", "B", "B", "G", "G"};
+		for(int i = 0; i < arr.Length; i++)
+		{
+			int num = UnityEngine.Random.Range(i,arr.Length);
+			if(i > 0)
+			{
+				while(arr[num] == arr[i-1])
+				{
+					num = UnityEngine.Random.Range(i,arr.Length);
+				}
+			}
+			string temp = arr[i];
+			arr[i] = arr[num];
+			arr[num] = temp;
+		}
+		string dropSequence = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+		Debug.Log(dropSequence);
+		
+		for(int i = 0; i < dropSequence.Length; i++)
+		{
+			char colour = dropSequence[i];
+			foreach(Transform child in colourLights.transform)
+			{
+				Light light = child.gameObject.GetComponent<Light>();
+				if(colour == 'R')
+				{
+					light.color = Color.red;
+				}
+				else if(colour == 'B')
+				{
+					light.color = Color.blue;
+				}
+				else
+				{
+					light.color = Color.green;
+				}
+			}
+			yield return new WaitForSeconds(0.5f);
+		}
+		foreach(Transform child in colourLights.transform)
+		{
+			Light light = child.gameObject.GetComponent<Light>();
+			light.color = Color.white;
+			light.intensity = 0.5f;
+		}
+		
+		yield return new WaitForSeconds(1);
+		
+		for(int i = 0; i < dropSequence.Length; i++)
+		{
+			char colour = dropSequence[i];
+			GameObject spikes;
+			if(colour == 'R')
+			{
+				spikes = redSpikes;
+			}
+			else if(colour == 'B')
+			{
+				spikes = blueSpikes;
+			}
+			else
+			{
+				spikes = greenSpikes;
+			}
+			
+			Vector3 startPos = spikes.transform.position;
+			while(true)
+			{
+				spikes.transform.position = Vector3.MoveTowards(spikes.transform.position, startPos + spikeDropDist, spikeDropSpeed*Time.deltaTime);
+				if((spikes.transform.position - (startPos + spikeDropDist)).magnitude <= 0.01)
+				{
+					spikes.transform.position = startPos + spikeDropDist;
+				}
+				if(spikes.transform.position == startPos + spikeDropDist)
+				{
+					break;
+				}
+				else
+				{
+					yield return null;
+				}
+			}
+			
+			startPos = spikes.transform.position;
+			while(true)
+			{
+				spikes.transform.position = Vector3.MoveTowards(spikes.transform.position, startPos - spikeDropDist, spikeDropSpeed*Time.deltaTime);
+				if((spikes.transform.position - (startPos - spikeDropDist)).magnitude <= 0.01)
+				{
+					spikes.transform.position = startPos - spikeDropDist;
+				}
+				if(spikes.transform.position == startPos - spikeDropDist)
+				{
+					break;
+				}
+				else
+				{
+					yield return null;
+				}
+			}
+		}
+		yield return new WaitForSeconds(0.5f);
+		colourLights.SetActive(false);
+	}
+	
+	IEnumerator laserCircleSpawn(int[] circleOrder, int orderNum)
+	{
+		GameObject laserCircle = laserCircles[circleOrder[orderNum]];
+		laserCircle.SetActive(true);
+		foreach(Transform child in laserCircle.transform)
+		{
+			yield return new WaitForSeconds(2);
+			/*while(child.eulerAngles.x > 0 && child.eulerAngles.x <= 90)
+			{
+				Debug.Log(child.eulerAngles.x);
+				yield return null;
+			}*/
+			if(orderNum < circleOrder.Length)
+			{
+				StartCoroutine(laserCircleSpawn(circleOrder, orderNum+1));
+			}
+			
+			yield return new WaitForSeconds(5);
+			laserCircle.SetActive(false);
+			break;
+		}
+		
+		yield return null;
+	}
+	
 	IEnumerator laserAttack1()
 	{
+		sweepingLasers1.SetActive(true);
 		Vector3 startPos = sweepingLasers1.transform.position;
 		while(true)
 		{
@@ -80,14 +285,16 @@ public class BattleManager : MonoBehaviour
 				yield return null;
 			}
 		}
+		sweepingLasers1.SetActive(false);
 	}
 	
 	IEnumerator laserAttack2()
 	{
+		sweepingLasers2.SetActive(true);
 		Vector3 startPos = sweepingLasers2.transform.position;
 		while(true)
 		{
-			sweepingLasers2.transform.position = Vector3.MoveTowards(sweepingLasers2.transform.position, startPos + lasers2Dist, lasers1Speed*Time.deltaTime);
+			sweepingLasers2.transform.position = Vector3.MoveTowards(sweepingLasers2.transform.position, startPos + lasers2Dist, lasers2Speed*Time.deltaTime);
 			if((sweepingLasers2.transform.position - (startPos + lasers2Dist)).magnitude <= 0.01)
 			{
 				sweepingLasers2.transform.position = startPos + lasers2Dist;
@@ -101,6 +308,50 @@ public class BattleManager : MonoBehaviour
 				yield return null;
 			}
 		}
+		sweepingLasers2.SetActive(false);
+	}
+	
+	IEnumerator laserAttack3()
+	{
+		sweepingLasers3.SetActive(true);
+		Vector3 startPos = sweepingLasers3.transform.position;
+		while(true)
+		{
+			sweepingLasers3.transform.position = Vector3.MoveTowards(sweepingLasers3.transform.position, startPos + lasers3Dist, lasers3Speed*Time.deltaTime);
+			if((sweepingLasers3.transform.position - (startPos + lasers3Dist)).magnitude <= 0.01)
+			{
+				sweepingLasers3.transform.position = startPos + lasers3Dist;
+			}
+			if(sweepingLasers3.transform.position == startPos + lasers3Dist)
+			{
+				break;
+			}
+			else
+			{
+				yield return null;
+			}
+		}
+		sweepingLasers3.SetActive(false);
+		sideLaser.SetActive(true);
+		
+		startPos = sideLaser.transform.position;
+		while(true)
+		{
+			sideLaser.transform.position = Vector3.MoveTowards(sideLaser.transform.position, startPos + sideLaserDist, sideLaserSpeed*Time.deltaTime);
+			if((sideLaser.transform.position - (startPos + sideLaserDist)).magnitude <= 0.01)
+			{
+				sideLaser.transform.position = startPos + sideLaserDist;
+			}
+			if(sideLaser.transform.position == startPos + sideLaserDist)
+			{
+				break;
+			}
+			else
+			{
+				yield return null;
+			}
+		}
+		sideLaser.SetActive(true);
 	}
 	
 	IEnumerator ramStab(int[] attackOrder, int orderNum)
@@ -145,7 +396,10 @@ public class BattleManager : MonoBehaviour
 			}
 		}
 		
-		StartCoroutine(ramStab(attackOrder, orderNum+1));
+		if(orderNum < attackOrder.Length)
+		{
+			StartCoroutine(ramStab(attackOrder, orderNum+1));
+		}
 		
 		startPos = ram.transform.position;
 		// battering ram retreats
